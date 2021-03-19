a0f6455c04d67907cabc50a119888d9ec6123ab2
Aisha Gench <aisha.gench@gmail.com>
2021-03-19 17:44:18 -0500

    Created new project Where am I

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+const latitudeInput = document.querySelector("#latitude");
+const longitudeInput = document.querySelector("#longitude");
+
+const apiKey = "";  // take yours !!!
+
+const whereAmI = function (lat, lng) {
+  
+};
+
+
+btn.addEventListener("click", displayCountry);
